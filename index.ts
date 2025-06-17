8946c4254e4bcbbddfb20611abc8e7269d6a0d2d
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2025-06-17 08:23:38 -0300

    atualização ultima aula

--- index.ts
+++ index.ts
@@ -1,52 +1,34 @@
-const express = require('express')
+import express from 'express';
+import {connect} from './db'
+import { Connection } from 'mysql2/promise';
+import { UserRepository } from './src/repository/usuario.repository';
+import { PlacaRepository } from './src/repository/placa.repository';
+import { usuarioRouter } from './routes/user.routes';
+import { placaRouter } from './routes/placa.routes';
 const app = express();
 app.use(express.json())
 
 const port = 3000;
+connect().then(db => api(db)).catch(err=>console.error("Falha ao conectar no mysql", err));
 
-let nextId =1;
-class User{
-  id:number
-  name:string
-  email: string
+
+// function validarNomeUsuario(req, res, next) {
+//   const { name } = req.body
+//   if (!name || typeof name !=='string' || name.trim() === '') {
+//     return res.status(400).json({erro: "O campo 'name' é obrigatório e deve ser uma string válida"})
+//   }
+//   next()
+
+function api(db:Connection) {
+  const userRepository = new UserRepository(db)
+  const placaRepository = new PlacaRepository(db)
   
+  app.use("/usuarios", usuarioRouter(userRepository))
+  app.use("/placa", placaRouter(placaRepository))
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}`)
+  })
+
 }
-
-function validarNomeUsuario(req, res, next) {
-  const { name } = req.body
-  if (!name || typeof name !=='string' || name.trim() === '') {
-    return res.status(400).json({erro: "O campo 'name' é obrigatório e deve ser uma string válida"})
-  }
-  next()
-}
-const users = []
-
-app.get('/', (_, res) => {
-  res.send('Hello World!')
-})
-
-app.get('/user/:id',(req,res) => {
-  const id = parseInt(req.params.id)
-  const user = users.find(u => u.id = id)
-  res.json(user)
-})
-
-app.get('/user', (_, res) => {
-   res.status(200).json(users)
-})
-
-app.post('/user',validarNomeUsuario,(req, res) => {
-
-    const { name } = req.body
-    if (!name) {
-      return res.status(400).json({ erro: "Nome é obrigatório" })
-    }
-    const newUser =new User(name)
-    users.push(newUser)
-    res.status(201).json({mensagem: "Usuário criado!", usuario: newUser})
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
 
